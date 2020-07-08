e007c5865e93b87e99bd19fe2d759a690f597f76
abbyswg <abbyswag25@gmail.com>
2020-07-08 14:11:48 +0530

    -first_commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const news = function(){
     fetch('http://newsapi.org/v2/top-headlines?country=us&apiKey=ceeb11c85e7547efbfa08bead2590516')
     .then(res=>res.json())
-    .then(Data=>console.log(data.status))
+    .then(data=>console.log(data.status))
 }
 news()
